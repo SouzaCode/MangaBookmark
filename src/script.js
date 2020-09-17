74fa60cac35ebc3ad7d798787c03f16a916275bc
Rodrigo Ferraz Souza <rodrigo.souza289@etec.sp.gov.br>
2020-09-17 14:04:10 -0300

    tentei montar uma lista mas parece que vou ter que usar reactjs

--- src/script.js
+++ src/script.js
@@ -1,8 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
-  document.querySelector("#btnadd").addEventListener("click", function () {
-    let text = document.querySelector("#ncap").textContent;
+  let a = document
+    .querySelector("button.op")
+    .addEventListener("click", function () {
+      alert("oi");
+    });
+
+  /*
+  document.querySelector("#btnadd1").addEventListener("click", function () {
+    let text = document.querySelector("#ncap1").textContent;
     let capn = parseInt(text.split(" ")[1]) ? parseInt(text.split(" ")[1]) : 0;
     capn += 1;
-    document.querySelector("#ncap").innerHTML = "Cap. " + capn;
+    document.querySelector("#ncap1").innerHTML = "Cap. " + capn;
   });
+  document.querySelector("#btnsub1").addEventListener("click", function () {
+    let text = document.querySelector("#ncap1").textContent;
+    let capn = parseInt(text.split(" ")[1]) ? parseInt(text.split(" ")[1]) : 0;
+    capn = capn - 1 > 0 ? capn - 1 : 0;
+    document.querySelector("#ncap1").innerHTML = "Cap. " + capn;
+  });
+  */
 });
