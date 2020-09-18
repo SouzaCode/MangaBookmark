484878959b89e43914a5b0f7177cbc82397e22c5
Rodrigo Ferraz Souza <rodrigo.souza289@etec.sp.gov.br>
2020-09-18 12:44:36 -0300

    coloquei um teste para fazer a ext usando react

---
+++ react-test/src/components/list/list.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./list.css";
+
+function List() {
+  return (
+    <div className="list-body">
+      <h1>oi, rodrigo!</h1>
+    </div>
+  );
+}
+
+export default List;
