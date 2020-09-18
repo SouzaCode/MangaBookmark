79df61b058417e8d243bb73444b3778b74c73049
Rodrigo Ferraz Souza <rodrigo.souza289@etec.sp.gov.br>
2020-09-18 13:38:00 -0300

    entendi como funciona o react

--- react-test/src/App.js
+++ react-test/src/App.js
@@ -1,12 +1,11 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import List from "./components/list/list";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <p>Meus Bookmarks</p>
       </header>
       <List />
     </div>
